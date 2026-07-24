6acd7ad9d2ed4e72386e8331263f80c2048e1209
epalzaluisesteban-sys <epalzaluisesteban@gmail.com>
2026-07-24 16:08:05 -0400

    cambie el title de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar Sesión - Sistema Residencial</title>
+    <title>Iniciar sesión</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="icon" href="{{ asset('imagen/tl.png') }}">
     @vite(['public/css/style.css', 'resources/js/app.js'])
